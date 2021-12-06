e57a3579599c700227bdb3dc838dde14845f133e
himnish17 <81815516+himnish17@users.noreply.github.com>
2021-12-06 07:31:36 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,25 @@
+status="";
+
+function preload() {
+    img=loadImage('image_jpg.jpeg');
+}
+
+function setup() {
+    canvas=createCanvas(640 , 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img , gotResults);
+}
+ function gotResults(error , results) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(results);
+        object=results;
+ }
